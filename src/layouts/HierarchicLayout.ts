4acfcfea2b45a6f407448132358d15a13d80379a
janerose_njogu <janerose.njogu@eoncollective.com>
2025-05-06 04:19:07 +0300

    function clean up, refactor, included node with in position calculation

--- src/layouts/HierarchicLayout.ts
+++ src/layouts/HierarchicLayout.ts
@@ -1,9 +1,4 @@
-import {
-  LayoutConfig,
-  LineageNode,
-  LineageEdge,
-  TraversalResult,
-} from "@/src/interfaces";
+import { LayoutConfig, TraversalResult } from "@/src/interfaces";
 import { Node, Edge } from "@xyflow/react";
 import { WeightHeuristic, LayoutOrientation } from "@/src/types";
 import { GraphTraversal } from "@/src/algorithms";
@@ -13,6 +8,7 @@ export class HierarchicLayout {
   private _graphEdges: Edge[];
   private _layoutConfig: LayoutConfig;
   private _layoutOrientation: LayoutOrientation;
+  private _connectedMap: Record<string, string[]>;
 
   private _gridSpacing: number;
   private _horizontalSpacing: number;
@@ -24,6 +20,7 @@ export class HierarchicLayout {
     this._graphNodes = nodes;
     this._graphEdges = edges;
     this._layoutConfig = layoutConfig;
+    this._connectedMap = {};
     this._layoutOrientation = layoutConfig.layoutOrientation;
     this._minimumLayerDistance = layoutConfig.minimumLayerDistance;
 
@@ -34,163 +31,26 @@ export class HierarchicLayout {
 
     this._weightHeuristic = "BARYCENTER";
   }
-  get layoutConfig(): LayoutConfig {
-    return this._layoutConfig;
+  get connectionMap(): Record<string, string[]> | undefined {
+    return this._connectedMap;
   }
-  set layoutConfig(config: LayoutConfig) {
-    this._layoutConfig = config;
-  }
-  get gridSpacing(): number {
-    return this._gridSpacing;
-  }
-  set gridSpacing(spacing: number) {
-    this._gridSpacing = spacing;
-  }
-  set layoutOrientation(orientation: LayoutOrientation) {
-    if (orientation !== "TB" && orientation !== "LR") {
-      throw new Error("Invalid layout orientation. Must be one of: TB or LR.");
-    }
-    this._layoutOrientation = orientation;
-  }
-  get layoutOrientation(): LayoutOrientation {
-    return this._layoutOrientation;
-  }
-  get weightHeuristic() {
-    return this._weightHeuristic;
-  }
-  set weightHeuristic(heuristic: WeightHeuristic) {
-    if (heuristic !== "BARYCENTER" && heuristic !== "MEDIAN") {
-      throw new Error("Invalid weight heuristic value.");
-    }
-    this._weightHeuristic = heuristic;
-  }
-  get minimumLayerDistance() {
-    // The minimum distance between adjacent layers
-    return this._minimumLayerDistance;
-  }
-  set minimumLayerDistance(minLayerDistance: number) {
-    // The minimum distance between adjacent layers
-    if (minLayerDistance < 0)
-      throw new Error("Negative minimum layer distance: " + minLayerDistance);
-    this._minimumLayerDistance = minLayerDistance;
-  }
-  get horizontalSpacing() {
-    // The horizontal distance between nodes in the same layer
-    return this._horizontalSpacing;
-  }
-  set horizontalSpacing(spacing: number) {
-    // The horizontal distance between nodes in the same layer
-    if (spacing < 0) throw new Error("Negative horizontal spacing: " + spacing);
-    this._horizontalSpacing = spacing;
-  }
-  get verticalSpacing() {
-    // The vertical distance between nodes in the same layer
-    return this._verticalSpacing;
-  }
-  set verticalSpacing(spacing: number) {
-    // The vertical distance between nodes in the same layer
-    if (spacing < 0) throw new Error("Negative vertical spacing: " + spacing);
-    this._verticalSpacing = spacing;
-  }
+  findAllConnectedElements(startId: string): Set<string> {
+    // finds all target nodes connected to the source node with the current id
+    const connectedElements = new Set<string>();
 
-  setGraphNodes(nodes: Node[]): void {
-    this._graphNodes = nodes;
-  }
-  setGraphEdges(edges: Edge[]): void {
-    this._graphEdges = edges;
-  }
-  get graphEdges(): LineageEdge[] {
-    return this._graphEdges;
-  }
-  get graphNodes(): LineageNode[] {
-    return this._graphNodes;
-  }
-  addNode(node: Node): void {
-    this._graphNodes.push(node);
-  }
-  addEdge(edge: Edge): void {
-    this._graphEdges.push(edge);
-  }
-  removeNode(nodeId: string): void {
-    this._graphNodes = this._graphNodes.filter((n) => n.id !== nodeId);
-    this._graphEdges = this._graphEdges.filter(
-      (e) => e.source !== nodeId && e.target !== nodeId
-    );
-  }
-  removeEdges(edgeId: string): void {
-    this._graphEdges = this._graphEdges.filter((e) => e.id !== edgeId);
-  }
-  updateNodes(updatedNode: Node): void {
-    this._graphNodes = this._graphNodes.map((n) =>
-      n.id === updatedNode.id ? updatedNode : n
-    );
-  }
-  updateEdges(updatedEdge: Edge): void {
-    this._graphEdges = this._graphEdges.map((e) =>
-      e.id === updatedEdge.id ? updatedEdge : e
-    );
-  }
-  executeLayout(): Record<
-    string,
-    {
-      x: number;
-      y: number;
-    }
-  > {
-    // TRAVERSE GRAPH
-    const traversal = new GraphTraversal();
-    const startNodes = traversal.findStartNodes(
-      this._graphNodes,
-      this._graphEdges
-    );
-    let traversalStack: TraversalResult = {
-      rootNode: null,
-      nodeChildren: new Map(),
-      nodeLayers: new Map(),
-    };
-    for (const startNode of startNodes) {
-      const partialTraversal = traversal.traverse(
-        startNode,
-        this._graphNodes,
-        this._graphEdges
-      );
-      if (!traversalStack.rootNode) {
-        traversalStack.rootNode = partialTraversal.rootNode;
+    this._graphEdges.forEach((edge) => {
+      if (edge.source === startId) {
+        connectedElements.add(edge.id);
+        connectedElements.add(edge.target);
       }
-      for (const [
-        parent,
-        children,
-      ] of partialTraversal.nodeChildren.entries()) {
-        if (!traversalStack.nodeChildren.has(parent)) {
-          traversalStack.nodeChildren.set(parent, []);
-        }
-        traversalStack.nodeChildren.get(parent)!.push(...children);
+      if (edge.target === startId) {
+        connectedElements.add(edge.id);
+        connectedElements.add(edge.source);
       }
+    });
 
-      // Merge nodeLayers
-      for (const [nodeId, layer] of partialTraversal.nodeLayers.entries()) {
-        if (!traversalStack.nodeLayers.has(nodeId)) {
-          traversalStack.nodeLayers.set(nodeId, layer);
-        }
-      }
-    }
-
-    // LAYERING - LAYER ORDERING
-    const layerMap = this.groupNodesByLayer(traversalStack.nodeLayers);
-
-    // const orderedLayers = this.getLayerIndexes(layerMap);
-
-    // const layerCount = this.getLayerCount(layerMap);
-
-    // NODE ORDERING WITHIN LAYERS
-    const orderedLayerMap = this.orderNodesWithinLayers(
-      layerMap,
-      traversalStack.nodeChildren
-    );
-
-    // ASSIGN NODE POSITIONS
-    const nodePositions = this.assignNodePositions(orderedLayerMap);
-    return nodePositions;
+    connectedElements.add(startId);
+    return connectedElements;
   }
   private groupNodesByLayer(
     nodeLayers: Map<string, number>
@@ -202,16 +62,18 @@ export class HierarchicLayout {
       }
       layerMap[layer].push(nodeId);
     }
+    // nodes that are not in any layer (i.e., not connected to any other node) are assigned to layer 0
+    const allNodeIds = this._graphNodes.map((node) => node.id);
+    for (const nodeId of allNodeIds) {
+      if (!nodeLayers.has(nodeId) && !this._connectedMap[nodeId]) {
+        if (!layerMap[0]) {
+          layerMap[0] = [];
+        }
+        layerMap[0].push(nodeId);
+      }
+    }
     return layerMap;
   }
-  // private getLayerIndexes(layerMap: Record<number, string[]>): number[] {
-  //   return Object.keys(layerMap)
-  //     .map(Number)
-  //     .sort((a, b) => a - b);
-  // }
-  // private getLayerCount(layerMap: Record<number, string[]>): number {
-  //   return Object.keys(layerMap).length;
-  // }
   private orderNodesWithinLayers(
     layerMap: Record<number, string[]>,
     nodeChildren: Map<string, string[]>
@@ -251,28 +113,100 @@ export class HierarchicLayout {
   }
   private assignNodePositions(orderedLayerMap: Record<number, string[]>) {
     const positions: Record<string, { x: number; y: number }> = {};
-    for (const [layerIndex, _] of Object.entries(orderedLayerMap)) {
+    const defaultNodeWidth = this._layoutConfig.nodeWidth;
+    const defaultNodeHeight = this._layoutConfig.nodeHeight;
+    for (const [layerIndex, nodeIds] of Object.entries(orderedLayerMap)) {
       const layerNum = Number(layerIndex);
-      const nodesInLayer = orderedLayerMap[layerNum];
 
-      for (let i = 0; i < nodesInLayer.length; i++) {
-        const nodeId = nodesInLayer[i];
+      for (let i = 0; i < nodeIds.length; i++) {
+        const nodeId = nodeIds[i];
+        const node = this._graphNodes.find((n) => n.id === nodeId);
+        const nodeWidth = node?.measured?.width ?? defaultNodeWidth;
+        const nodeHeight = node?.measured?.height ?? defaultNodeHeight;
 
         let x = 0;
         let y = 0;
 
-        if (this._layoutOrientation === "LR") {
-          x = i * this._horizontalSpacing;
-          y = layerNum * (this._verticalSpacing + this._minimumLayerDistance);
-        } else if (this._layoutOrientation === "TB") {
-          x = layerNum * (this._horizontalSpacing + this._minimumLayerDistance);
-          y = i * this._verticalSpacing;
+        if (this._layoutOrientation === "TB") {
+          x = i * (nodeWidth + this._horizontalSpacing);
+          y =
+            layerNum *
+            (nodeHeight + this._verticalSpacing + this._minimumLayerDistance);
+        } else if (this._layoutOrientation === "LR") {
+          x =
+            layerNum *
+            (nodeWidth + this._horizontalSpacing + this._minimumLayerDistance);
+          y = i * (nodeHeight + this._verticalSpacing);
         }
-
         positions[nodeId] = { x, y };
       }
     }
 
     return positions;
+  }
+  executeLayout(): Record<
+    string,
+    {
+      x: number;
+      y: number;
+    }
+  > {
+    // TRAVERSE GRAPH
+    const traversal = new GraphTraversal();
+    const startNodes = traversal.findStartNodes(
+      this._graphNodes,
+      this._graphEdges
+    );
+    let traversalStack: TraversalResult = {
+      rootNode: null,
+      nodeChildren: new Map(),
+      nodeLayers: new Map(),
+    };
+    for (const startNode of startNodes) {
+      const partialTraversal = traversal.traverse(
+        startNode,
+        this._graphNodes,
+        this._graphEdges
+      );
+      if (!traversalStack.rootNode) {
+        traversalStack.rootNode = partialTraversal.rootNode;
+      }
+      for (const [
+        parent,
+        children,
+      ] of partialTraversal.nodeChildren.entries()) {
+        if (!traversalStack.nodeChildren.has(parent)) {
+          traversalStack.nodeChildren.set(parent, []);
+        }
+        traversalStack.nodeChildren.get(parent)!.push(...children);
+      }
+      // Merge nodeLayers
+      for (const [nodeId, layer] of partialTraversal.nodeLayers.entries()) {
+        if (!traversalStack.nodeLayers.has(nodeId)) {
+          traversalStack.nodeLayers.set(nodeId, layer);
+        }
+      }
+    }
+    this._graphEdges.forEach((edge) => {
+      if (!this._connectedMap[edge.source]) {
+        this._connectedMap[edge.source] = [];
+      }
+      if (!this._connectedMap[edge.target]) {
+        this._connectedMap[edge.target] = [];
+      }
+      this._connectedMap[edge.source].push(edge.target);
+      this._connectedMap[edge.target].push(edge.source);
+    });
+    // LAYERING - LAYER ORDERING
+    const layerMap = this.groupNodesByLayer(traversalStack.nodeLayers);
+    // NODE ORDERING WITHIN LAYERS
+    const orderedLayerMap = this.orderNodesWithinLayers(
+      layerMap,
+      traversalStack.nodeChildren
+    );
+    // ASSIGN NODE POSITIONS
+    const nodePositions = this.assignNodePositions(orderedLayerMap);
+    console.log("Node Positions", nodePositions);
+    return nodePositions;
   }
 }
